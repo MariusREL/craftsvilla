79a2868af4d74ab2f766dd25c976543086b689e6
Marius R <Mrel0425@gmail.com>
2025-07-24 09:40:40 +0200

    Refactor index.js for consistent formatting and improved readability

--- index.js
+++ index.js
@@ -1,42 +1,42 @@
-const navbar = document.querySelector('.navbar');
-const headerImg = document.querySelector('.header-img');
-const logo = document.querySelector("#logo")
+const navbar = document.querySelector(".navbar");
+const headerImg = document.querySelector(".header-img");
+const logo = document.querySelector("#logo");
 
+document.addEventListener("scroll", () => {
+  const headerBottom = headerImg.offsetTop + headerImg.offsetHeight;
 
-document.addEventListener('scroll', () => {
-    const headerBottom = headerImg.offsetTop + headerImg.offsetHeight;
-    
-    if (window.scrollY >= headerBottom) {
-        navbar.classList.add('nav-activated');
-        logo.setAttribute("src", "./img/CraftsvillaB.png")
-
-    } else {
-        navbar.classList.remove('nav-activated')
-        logo.setAttribute("src", "./img/CraftsvillaW.png");
-    }
+  if (window.scrollY >= headerBottom) {
+    navbar.classList.add("nav-activated");
+    logo.setAttribute("src", "./img/CraftsvillaB.png");
+  } else {
+    navbar.classList.remove("nav-activated");
+    logo.setAttribute("src", "./img/CraftsvillaW.png");
+  }
 });
 
-function imagetransition(){
-    let images = document.querySelectorAll(".background-image-header")
-    let currentIndex = 0;
+function imagetransition() {
+  let images = document.querySelectorAll(".background-image-header");
+  let currentIndex = 0;
 
-    for (let i = 0; i < images.length; i++) {
-        images[i].style.opacity = i === 0 ? 1 : 0;
-    }
-    setInterval(() => {
-        images[currentIndex].style.opacity = 0
-        currentIndex = (currentIndex + 1) % images.length;
-        images[currentIndex].style.opacity = 1;
-    }, 5000);
+  for (let i = 0; i < images.length; i++) {
+    images[i].style.opacity = i === 0 ? 1 : 0;
+  }
+  setInterval(() => {
+    images[currentIndex].style.opacity = 0;
+    currentIndex = (currentIndex + 1) % images.length;
+    images[currentIndex].style.opacity = 1;
+  }, 5000);
 }
 document.addEventListener("DOMContentLoaded", () => {
-    document.querySelectorAll('img:not([loading])').forEach(img => {
-        img.setAttribute('loading', 'lazy');
-    });
-    imagetransition()
-    setTimeout(() => {
-        document.querySelectorAll('.background-image-header[data-src]').forEach(img => {
-            img.src = img.dataset.src;
-        });
-    }, 500); 
+  document.querySelectorAll("img:not([loading])").forEach((img) => {
+    img.setAttribute("loading", "lazy");
+  });
+  imagetransition();
+  setTimeout(() => {
+    document
+      .querySelectorAll(".background-image-header[data-src]")
+      .forEach((img) => {
+        img.src = img.dataset.src;
+      });
+  }, 500);
 });
